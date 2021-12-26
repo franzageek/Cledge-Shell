ba39bdf4a9b360403b43ba17992f4a303aee029a
StuckDuck <71439748+StuckDuck7710@users.noreply.github.com>
2021-12-26 18:58:37 +0100

    Update cledge.ps1

--- src/cledge.ps1
+++ src/cledge.ps1
@@ -1,4 +1,13 @@
 # Cledge Shell v1.9
 # © 2021, SwiftDevLabs Open Source
 
-# Start File
+if ($args.length -gt 1) {
+  for ($arg -in $args) {
+    Invoke-Expression -Command $arg
+  }
+} else {
+  $arg = Read-Host 'Write a command'
+  while (-not $arg -eq '') {
+    Invoke-Expression -Command $arg
+  }
+}
